afc3fe257046f094966ccabc765bee8fc481ca6f
tydeu <tydeu@hatpress.net>
2022-07-06 14:46:52 -0400

    chore: add license

--- Alloy/C/Emit.lean
+++ Alloy/C/Emit.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2022 Mac Malone. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Mac Malone
+-/
 import Alloy.Util.Emit
 import Alloy.C.Extension
 import Lean.Compiler.IR.EmitC
