5ae885f01218145ac2aac9bc2dd66b99dbb686d3
tydeu <tydeu@hatpress.net>
2024-02-14 19:59:25 -0500

    chore: update to Lean v4.6.0-rc1

--- Alloy/Util/Command.lean
+++ Alloy/Util/Command.lean
@@ -13,7 +13,7 @@ open Lean Elab Command
   .node (.ofCommandInfo { elaborator, stx }) trees
 
 /-- Create context info for the info tree from the current state of command elaboration.  -/
-def mkCommandElabContextInfo : CommandElabM ContextInfo := do
+def mkCommandElabContextInfo : CommandElabM CommandContextInfo := do
   let ctx ← read; let s ← get; let scope := s.scopes.head!
   return  {
     env := s.env, fileMap := ctx.fileMap, mctx := {}, currNamespace := scope.currNamespace,
@@ -22,7 +22,7 @@ def mkCommandElabContextInfo : CommandElabM ContextInfo := do
 
 /-- Create an info tree for an elaborator with the current command elaboration context. -/
 @[inline] def mkCommandElabInfoTree (elaborator : Name) (stx : Syntax) (trees : PersistentArray InfoTree) : CommandElabM InfoTree := do
-  return .context (← mkCommandElabContextInfo) (mkElabInfoTree elaborator stx trees)
+  return .context (.commandCtx (← mkCommandElabContextInfo)) (mkElabInfoTree elaborator stx trees)
 
 /-- Adapted from the private `elabCommandUsing` definition in `Lean.Elab.Command`. -/
 def elabCommandUsing (stx : Syntax) : List (KeyedDeclsAttribute.AttributeEntry CommandElab) → CommandElabM Bool
